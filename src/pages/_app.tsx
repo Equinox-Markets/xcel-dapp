e4f60eefebb678bb9f22c6c022d16b630fb40b1c
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2022-10-20 07:55:50 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ import { theme } from '@/lib/theme';
 import { ETHProvider } from '../components/EthProvider';
 import { getLibrary } from '../lib/web3-react';
 import { WalletSelecter } from '../components/WalletSelecter/index';
-import Footer from '../components/Footer';
+import Footer from '../components/footer';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
